782686b85ae04ecb11211c217b850a81cc1fa5a1
Vu Phan <vu_phan_chau@bita.jp>
2022-06-23 12:00:51 +0700

    edit index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello world!</h1>
+  <h1>Hello world bitA!!!</h1>
 </template>
 
 <script lang="ts">
@@ -7,5 +7,16 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  async asyncData ({ $axios }) {
+    try {
+      console.log('index page')
+      console.log('BASE_URL: ',process.env.BASE_URL)
+      const response = await $axios.$get(process.env.BASE_URL + `/rcms-api/8/form/3`)
+      console.log(response)
+      // return { response }
+    }catch (e) {
+      // console.log(e.message)
+    }
+  }
 })
 </script>
